b8697e0947391a99aba581e9edf2922807105abe
Pankaj Garg <pankajg@microsoft.com>
2019-01-29 21:20:25 -0800

    Fix

--- travis_bld.sh
+++ travis_bld.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-if [[ "${ETH_LATEST}" = "true" ]] && [[ "${ETH_LINUX}" = "true" ]]; then
-    local GOOS=windows
-    local GOARCH=amd64
+echo $TRAVIS_OS_NAME
+echo "${TRAVIS_OS_NAME}"
+echo "${TRAVIS_GO_VERSION}"
+if [[ "${TRAVIS_OS_NAME}" = "linux" ]] && [[ ${TRAVIS_GO_VERSION} = 1.11* ]]; then
+    GOOS=windows
+    GOARCH=amd64
     go build -o windows/ethr.exe
     GOOS=linux
     go build -o linux/ethr
     GOOS=darwin
     go build -o darwin/ethr
 fi
+
