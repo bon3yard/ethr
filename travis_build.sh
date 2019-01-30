a5e3c9f0cff6e1962e42f0a627d69c3b4ffbe6fd
Pankaj Garg <pankajg@microsoft.com>
2019-01-30 03:57:48 -0800

    v0.4

--- travis_build.sh
+++ travis_build.sh
@@ -3,12 +3,12 @@ echo $TRAVIS_OS_NAME
 echo "${TRAVIS_OS_NAME}"
 echo "${TRAVIS_GO_VERSION}"
 if [ "${TRAVIS_OS_NAME}" = "linux" ] && [[ ${TRAVIS_GO_VERSION} == 1.11* ]]; then
-    GOOS=windows
-    GOARCH=amd64
+    export GOOS=windows
+    export GOARCH=amd64
     go build -o windows/ethr.exe
-    GOOS=linux
+    export GOOS=linux
     go build -o linux/ethr
-    GOOS=darwin
+    export GOOS=darwin
     go build -o osx/ethr
     zip ethr_windows.zip windows/ethr.exe
     zip ethr_linux.zip linux/ethr
